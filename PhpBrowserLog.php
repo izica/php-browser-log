149521aa04a0be07af8f8aa97389be12d1f2b4a6
izica <artemiztomska@gmail.com>
2018-06-10 14:37:34 +0700

    initial

---
+++ PhpBrowserLog.php
@@ -0,0 +1,36 @@
+<?php
+class PhpBrowserLog
+{
+    public static function script($data, $command = 'console.log', $variable = false)
+    {
+        echo '<script>';
+        if ($variable === false) {
+            echo $command . '(' . json_encode($data) . ')';
+        } else {
+            echo $command . '("' . trim($variable) . '",' . json_encode($data) . ')';
+        }
+        echo '</script>';
+    }
+
+    public static getVarsNames($arCaller)
+    {
+        $sFile = file_get_contents($arCaller['file']);
+        $arFile = explode("\n", $sFile);
+        $arArgsNames = explode('(', $arFile[$arCaller[line] - 1])[1];
+        $arArgsNames = explode(')', $arArgsNames)[0];
+        return explode(',', $arArgsNames);
+    }
+
+    public static print()
+    {
+        $arBt = debug_backtrace();
+        $arCaller = array_shift($arBt);
+        $arArgsNames = self::getVarsNames($arCaller);
+
+        self::script('[' . $arCaller['line'] . '] ' . $arCaller['file'], 'console.groupCollapsed');
+        foreach ($arCaller['args'] as $nKey => $anyArg) {
+            self::script($anyArg, 'console.log', $arArgsNames[$nKey]);
+        }
+        self::script('', 'console.groupEnd');
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# php-browser-log
+## Usage
+You need to use this debug in html view.
+You will see result in browser console(F12, Inspect mode console);
+```php
+    PhpBrowserLog::print($arg1, $arg2);
+```
